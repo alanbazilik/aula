56447aa43b3c9ef6fb0d71057a63a7693967e10e
alanbazilik <alangoncalvesbazilio@gmail.com>
2022-06-02 22:04:50 -0300

    aula

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,16 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { IonicStorageModule } from '@ionic/storage-angular';
 import { FormsModule } from '@angular/forms';
+import { DatePipe } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), FormsModule, AppRoutingModule, IonicStorageModule.forRoot({
     name: '__mydb',
-       driverOrder: ['indexeddb', 'sqlite', 'websql']
+       //driverOrder: ['indexeddb', 'sqlite', 'websql']
   })],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },DatePipe],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/contact-add/contact-add.page.ts
+++ src/app/contact-add/contact-add.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Contact } from '../services/storage.service';
+import { Contact, StorageService } from '../services/storage.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-contact-add',
@@ -9,12 +10,26 @@ import { Contact } from '../services/storage.service';
 export class ContactAddPage implements OnInit {
   model: any = [];
   key: string;
-  constructor() { }
+  contacts: any = [];
+  constructor(private storageService: StorageService,
+  private toast: ToastController
+    ) { }
 
   ngOnInit() {
+    this.getAllContacts();
   }
   save(){
-
+ this.storageService.insert(this.model).then(() => {
+   console.log("salvo com sucesso");
+ }).catch(() => {
+   console.log("Erro");
+ })
+  }
+  getAllContacts(){
+    this.storageService.getAll().then((res) => {
+      console.log(res);
+      this.contacts = res;
+    })
   }
 
 }
